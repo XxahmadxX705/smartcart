712d1fd4597c2ec46af1ac733c82bfc65abde689
XxahmadxX705 <ahm4d.w0rk.90@gmail.com>
2026-06-25 15:23:12 +0300

    Day 3: add-to-cart with item totals

---
+++ javascript/cart.js
@@ -0,0 +1,17 @@
+let cart = [];
+
+export function addToCart(product) {
+    cart.push(product);
+}
+
+
+export function getcart() {
+    return cart;
+}
+
+
+export function gettotal() {
+    return cart.reduce((sum, product) => sum + product.price, 0);
+}
+
+
--- javascript/ui.js
+++ javascript/ui.js
@@ -1,3 +1,5 @@
+import {addToCart , getcart, gettotal} from './cart.js';
+
 export function renderProducts(products) {
     const productlist = document.getElementById('product-list');
 
@@ -9,21 +11,43 @@ export function renderProducts(products) {
             <h3>${product.title}</h3>
             <p>Price: $${product.price}</p>
             <p>${product.category}</p>
+            <button>Add to Cart</button>
         `;
+
+        const button = card.querySelector('button');
+        button.addEventListener('click', () => {
+            addToCart(product);
+            renderCart();
+            alert(`${product.title} has been added to the cart!`);
+        });
+
         productlist.appendChild(card);
     });
+}
+export function renderCart() {
+    const cartitem = document.getElementById('cart-items');
+    cartitem.innerHTML =``;
+
+    getcart().forEach(product => {
+        const item = document.createElement('div');
+        item.innerHTML = ` ${product.title} - $${product.price}`;
+        cartitem.appendChild(item);
+    });
+
+    document.getElementById('cart-total').innerText = `Total: $${gettotal().toFixed(2)}`;
 }
 
 export function setupnavigation() {
     const menuitem  = document.querySelectorAll('.menu-item');
     const views = document.querySelectorAll('.view');
 
+
     menuitem.forEach(item => {
         item.addEventListener('click', () => {
             menuitem.forEach(view => {
                 view.classList.remove('active');
             })
-            
+
             item.classList.add('active');
 
             views.forEach(view => {
